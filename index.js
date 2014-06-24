071fd0ef08b708314c29f3dc2c2a8a1fdc015017
chencheng <sorrycc@gmail.com>
2014-06-24 18:43:42 +0800

    0.1.2, filter no exist files

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var uniq = require('uniq');
 var gutil = require('gulp-util');
 var $ = require('cheerio');
 var format = require('util').format;
+var dirname = require('path').dirname;
+var join = require('path').join;
+var exists = require('fs').existsSync;
 
 module.exports = function(fn) {
 
   return through.obj(function(file, enc, callback) {
     var contents = file.contents.toString();
     var images = getImages(contents);
+    images = images.filter(function(image) {
+      return exists(join(dirname(file.path), image));
+    });
+
     fn(images, function(e, urls) {
       if (e) {
         return callback(new gutil.PluginError('cssimg', e));
